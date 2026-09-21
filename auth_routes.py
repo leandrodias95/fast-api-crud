82e3c674788cfb5d11c45aa1677e85b44f65b3c9
leandrodias95 <leandrogit95@gmail.com>
2026-09-21 11:36:29 -0300

    creation manually token for authentication

--- auth_routes.py
+++ auth_routes.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException
 from models import User
 from dependencies import get_db_session
-from security import hash_password
+from main import bcrypt_context
 from schemas import UserSchema, LoginSchema
 from sqlalchemy.orm import Session
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
@@ -9,6 +9,14 @@ auth_router = APIRouter(prefix="/auth", tags=["auth"])
 def create_token(id: str):
     token = f"a95c4b209ff9de1b823788c95aa33f0e4f121e7e609579d552a9ea62f3a948bc{id}"
     return token
+
+def authenticate_user(email: str, password: str, session: Session):
+    user = session.query(User).filter(User.email == email).first()
+    if not user:
+        return False
+    elif not bcrypt_context.verify(password, user.password):
+        return False
+    return user
 
 @auth_router.get("/")
 async def home():
@@ -22,7 +30,7 @@ async def register(user_schema: UserSchema, session: Session = Depends(get_db_se
     if user:
         raise HTTPException(status_code=400, detail="Usuário já existe")
     else:
-        crypted_password = hash_password(user_schema.password)
+        crypted_password = bcrypt_context.hash(user_schema.password)
         new_user = User(user_schema.name, user_schema.email, crypted_password)
         session.add(new_user)
         session.commit()
@@ -31,9 +39,9 @@ async def register(user_schema: UserSchema, session: Session = Depends(get_db_se
 
 @auth_router.post("/login")
 async def login(login: LoginSchema, session: Session = Depends(get_db_session)):
-    user = session.query(User).filter_by(email=login.email).first()
+    user = authenticate_user(login.email, login.password, session)
     if not user:
-        raise HTTPException(status_code=400, detail="Credenciais inválidas")
+        raise HTTPException(status_code=400, detail="Usuário não encontrado ou credenciais inválidas")
     else:
         access_token = create_token(user.id)
         return {"access_token": access_token, "token_type": "Bearer"}
--- main.py
+++ main.py
@@ -2,15 +2,18 @@
 ##uvicorn main:app --reload  
 
 from fastapi import FastAPI
-from auth_routes import auth_router
-from order_routes import order_router
 from dotenv import load_dotenv
+from passlib.context import CryptContext
 import os
-
 
 load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY")
+
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+from auth_routes import auth_router
+from order_routes import order_router
 
 app = FastAPI()
 
